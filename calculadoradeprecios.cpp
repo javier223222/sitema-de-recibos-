1fadeb7bc87133708e541fbc3122d3afc0a1d355
javier <jcundapi12345678@gmail.com>
2022-09-14 08:24:33 -0500

    agregamos el primer codigo

--- calculadoradeprecios.cpp
+++ calculadoradeprecios.cpp
@@ -1,2 +1,26 @@
 #include <iostream>
 using namespace std;
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+int main()
+{
+ int totalpanes;
+ cout<<"cuentas los panes\n";
+ cout<<"ingrese el total de panes\n";
+  int precio,suma=0,cantidad=0;
+  do
+  {
+    cout <<"ingres el precio \n";
+    cin>>precio;
+    if (precio>0)
+    {
+        suma=suma+precio;
+        cantidad++;
+    }
+    
+  } while (cantidad <=5);
+  cout << "total a pagar " << suma;
+
+}
