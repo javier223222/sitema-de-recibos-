381373230cca818dfa9d37ad9f3309213be68b05
javier <jcundapi12345678@gmail.com>
2022-09-14 08:32:56 -0500

    agregamos los numero aleatorios

--- calculadoradeprecios.cpp
+++ calculadoradeprecios.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
 using namespace std;
+#include <cstdlib> 
+#include <ctime>
+using namespace std;
+int main(){
+     srand(time(NULL));
+  for (int i = 0; i < 20; i++)
+  {
+    numero= 1 + rand()%50;
+    cout<< numero;
+  }
+  
+}
