e940622e84ce6f0562418bcbe7bfb5f4fe9f3365
javier <jcundapi12345678@gmail.com>
2022-09-14 08:46:30 -0500

    correccion de   error

--- calculadoradeprecios.cpp
+++ calculadoradeprecios.cpp
@@ -21,7 +21,7 @@ int main()
         cantidad++;
     }
     
-  } while (cantidad =totalpanes);
+  } while (cantidad <totalpanes);
    int numero;
     srand(time(NULL));
   for (int i = 0; i < 20; i++)
